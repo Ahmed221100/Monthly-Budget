67e0d8d0553ddf66dc4947951400c61577cf3e89
omar mahdy <betav330@gmail.com>
2025-01-30 12:17:40 +0200

    Update index.js
    rollback to old verification

--- Js/index.js
+++ Js/index.js
@@ -9,6 +9,23 @@ userInputSubmitBtn.addEventListener("click", (e) => {
     alert("please fill in all the inputs");
     return;
   }
+  if (isNaN(+userIncome.value) || Number(+userIncome.value) <= 0) {
+    console.log(userIncome.value);
+    alert("Please enter a valid and positive income value.");
+    return false;
+  }
+
+  if (!/^[a-zA-Z\s]+$/.test(userName)) {
+    alert("Please enter a valid names.");
+    return false;
+  }
+  if (!/^[a-zA-Z0-9\s,]+$/.test(userGoal.value)) {
+    alert(
+      "Please enter valid targets that contain only letters, numbers, and commas."
+    );
+    return false;
+  }
+
   localStorage.setItem("user-income", userIncome.value);
   localStorage.setItem("user-name", userName.value);
   localStorage.setItem("user-goal", userGoal.value);
